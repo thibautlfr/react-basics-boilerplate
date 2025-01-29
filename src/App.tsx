be67d6e37f716bd016fbbcb9058361b76ba8e126
Thibaut Lefrancois <thibaut.lfr@gmail.com>
2025-01-29 20:46:39 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="flex bg-gray-700 justify-center items-center h-screen w-screen">
+        <h1 className="text-4xl text-white font-bold">
+          Let's dev with React and TailwindCSS!
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.ts
@@ -0,0 +1,2 @@
+// Not to be completed since the Tailwind V4.0.
+// This file is only used to trigger the Tailwind CSS IntelliSens extension
